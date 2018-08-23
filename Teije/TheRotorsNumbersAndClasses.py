1c8da2dc501f45269e3422542c290f2faafd0f10
Teije Onstein <Poseidonst>
2018-08-23 20:45:52 +0200

    Tuning

--- Teije/TheRotorsNumbersAndClasses.py
+++ Teije/TheRotorsNumbersAndClasses.py
@@ -1,7 +1,6 @@
 alphabet_list = [i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 alphabet_dict = {chr(65+i) : i for i in range(26)}
 rotor_I_numbers = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
-rotor_I_numbers_backup = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
 rotor_II_numbers = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
 
 def make_letters():
@@ -52,7 +51,6 @@ class RotorClass(object):
         for i in range(0, places):
             self.listname += [self.listname.pop(0)]
         self.counter = 0
-
 
 rotor_I = RotorClass(rotor_I_numbers, "I", 0)
 
