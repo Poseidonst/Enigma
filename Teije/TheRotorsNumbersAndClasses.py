9d47ba55c7c56a5b26feecd0b20c1a4496f36a23
Teije Onstein <Poseidonst>
2018-08-23 18:27:32 +0200

    Made the classes better

--- Teije/TheRotorsNumbersAndClasses.py
+++ Teije/TheRotorsNumbersAndClasses.py
@@ -19,10 +19,11 @@ def switch_rotor(rotor, position):
 
 class RotorClass(object):
 
-    def __init__(self, listname, backup, name):
+    def __init__(self, listname, backup, name, counter):
         self.listname = listname
         self.backup = backup
         self.name = name
+        self.counter = counter
 
     def switch(self, places):           #Switches and goes back to starting position immediately
         new_rotor = []
@@ -35,21 +36,37 @@ class RotorClass(object):
             new_rotor.append(self.listname[i])
         return (new_rotor)
 
-    def defswitch(self, places):        #Switches and stays at that position
+    def defswitch(self, places):
+        while places < 0:               #This way it works with negative numbers as well
+            places = places + 26
+        while places > 26:
+            places = places - 26       #Switches and stays at that position
         for i in range(0, places):
             self.listname += [self.listname.pop(0)]
+            self.counter += 1
+            if self.counter == 27:
+                self.counter = 1
         return (self.listname)
 
     def reset(self):                    #Resets back to starting position
-        self.listname = self.backup
-        return(self.listname)
+        places = 26 - self.counter
+        for i in range(0, places):
+            self.listname += [self.listname.pop(0)]
+        self.counter = 0
 
-rotor_I = RotorClass(rotor_I_numbers, rotor_I_numbers_backup, "I")
+
+rotor_I = RotorClass(rotor_I_numbers, rotor_I_numbers_backup, "I", 0)
 
 #print(rotor_I.listname)
 #print(rotor_I.name)
 #print(rotor_I.switch(1))
-#print(rotor_I.defswitch(1))
-#print(rotor_I.defswitch(1))
-#print(rotor_I.reset())
-#print(rotor_I.defswitch(1))
+print(rotor_I.counter)
+print(rotor_I.defswitch(-52))
+print(rotor_I.counter)
+print(rotor_I.defswitch(1))
+print(rotor_I.counter)
+rotor_I.reset()
+print(rotor_I.counter)
+print(rotor_I.defswitch(1))
+print(rotor_I.counter)
+#print(rotor_I.get_position())
