bdedd5f09360938cff78daef69da4d2eb1eb0550
Teije Onstein <Poseidonst>
2018-08-24 18:20:56 +0200

    Entire enigma program 2 rotors complete, have to make function for other segments

--- Teije/EnigmaMachineUsingClasses.py
+++ Teije/EnigmaMachineUsingClasses.py
@@ -40,6 +40,13 @@ class RotorClass(object):
                 self.position = 1
         return (self.listname)
 
+    def switch1(self):
+        self.listname += [self.listname.pop(0)]
+        self.position += 1
+        if self.position == 27:
+            self.position = 1
+        return (self.listname)
+
     def reset(self):                    #Resets back to starting position
         places = 26 - self.position
         for i in range(0, places):
@@ -50,26 +57,26 @@ def get_difference(rotor1, rotor2):
     difference = rotor2.position - rotor1.position
     return(difference)
 
-def enigma():
-    userinput = "AAAAA"              #str(input("Give a letter: ")).upper()
+def enigma(userinput, userrotor1, userrotor2):             #userinput is the text to code, userrotor1 is the rotorposition of first rotor, and userrotor2 for the second rotor
     userinputlist = [i for i in userinput]
     codedlist = []
-    userrotor1 = 0
-    userrotor2 = 1
+    rotorI.defswitch(userrotor1)
+    rotorII.defswitch(userrotor2)
     for i in userinputlist:
-        rotorI.defswitch(userrotor1)
-        rotorII.defswitch(userrotor2)
         i = alphabet_dict[i]
         diff = get_difference(rotorI, rotorII)
         i = rotorI.listname[i]
         i += diff
         i = rotorII.listname[i - rotorII.position]
         codedlist.append(alphabet_list[i])
-        userrotor1 += 1
-    for i in codedlist:
-        print(i, end = "")
-
+        rotorI.switch1()
+        if rotorI.position == rotorI.rotortip:
+            rotorII.switch1()
+    return("".join(codedlist))
 
 rotorI = RotorClass(rotor_I_numbers, "I", 0, 7)
 rotorII = RotorClass(rotor_II_numbers, "II", 0, 8)
-enigma()
+
+for i in range(0, 25):
+    for j in range(0, 25):
+        print(enigma("AAAAA", i, j))
